ecbd397f758cf5dd5d08b2cc4c0adeb512107567
Berin <58124@etu.he2b.be>
2026-01-14 22:25:37 +0100

    add(routes) : pages auth

---
+++ frontend-cible/src/routes/auth/login.tsx
@@ -0,0 +1,19 @@
+
+
+function LoginPage() {
+    return (
+        <div>
+            <div>
+                <p>Login</p>
+                <input />
+            </div>
+            <div>
+                <p>Password</p>
+                <input />
+            </div>
+        </div>
+    );
+}
+
+
+export default LoginPage;
