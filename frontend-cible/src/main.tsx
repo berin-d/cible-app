c5fd4b3b8a188b2670232d6de286d2efbd95b278
Berin <58124@etu.he2b.be>
2026-01-15 14:23:13 +0100

    add(login)

--- frontend-cible/src/main.tsx
+++ frontend-cible/src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import LoginPage from "./routes/auth/login";
+import "./App.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <LoginPage/>
   </React.StrictMode>,
 );
--- frontend-cible/src/routes/auth/login.tsx
+++ frontend-cible/src/routes/auth/login.tsx
@@ -1,15 +1,21 @@
 
-
 function LoginPage() {
     return (
-        <div>
-            <div>
-                <p>Login</p>
-                <input />
+        <div className="flex flex-col items-center justify-center w-screen h-screen l bg-[#1A1A1E] gap-2 p-5">
+            <div className="text-white">
+                <p>Username</p>
+                <input 
+                className="bg-[#3B3B3F] border-0 border-emerald-500 rounded-md p-2"
+                placeholder="Enter your username.."/>
+            </div>
+            <div className="text-white">
+                <p>Password</p>
+                <input 
+                className="bg-[#3B3B3F] border-0 border-emerald-500 rounded-md p-2 text-white" 
+                placeholder="Enter your password..."/> 
             </div>
             <div>
-                <p>Password</p>
-                <input />
+                <button className="bg-[#50C878] border-0 rounded-md p-1 pl-5 pr-5 hover:bg-[#2c7544]">Login</button>
             </div>
         </div>
     );
