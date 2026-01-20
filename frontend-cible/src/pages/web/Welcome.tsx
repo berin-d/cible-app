ec92879928e62cd6669825a722f332fc5d8df4fd
Berin <berin358@outlook.fr>
2026-01-20 21:04:08 +0100

    update(html) : welcome page

--- frontend-cible/src/pages/web/Welcome.tsx
+++ frontend-cible/src/pages/web/Welcome.tsx
@@ -1,7 +1,29 @@
+import Button from "../../components/commons/button"
+import logo from "../../assets/logo-cible.png"
 
-
-export default function WelcomePage(){
+export default function WelcomePage() {
     return (
-        <p>Welcome page</p>
+        <div >
+            <header className="flex justify-end bg-[#242429] text-white p-2">
+                <nav className="flex gap-2 items-center p-2">
+                    <Button text="Download"></Button>
+                    <a>Login</a>
+                </nav>
+            </header>
+
+            <main className="bg-[#1A1A1E] h-screen">
+                <div>
+
+                    <div className="flex flex-col justify-center items-center">
+                        <img src={logo} alt="logo" className="size-64"></img>
+                        <h1 className="text-white text-4xl text-center mt-4 font-bold">Turn Tasks Into Progress</h1>
+                        <Button text="Get Started" ></Button>
+                    </div>
+
+                </div>
+            </main>
+
+        </div>
+
     )
 }
